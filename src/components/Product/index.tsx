aefa8a75d0572c07b3ecfd8b2c3e47ce41936d6f
jiyuujin <jiyuujinunite@gmail.com>
2021-12-15 10:52:50 +0900

    show sub products

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -7,9 +7,14 @@ type ProductProps = {
       id: string
       title: string
       description: string
-      skills: string[]
-      image: string
-      url: string
+      skills?: string[]
+      image?: string
+      url?: string
+      sub?: Array<{
+        title: string
+        skills: string[]
+        url: string
+      }>
     }
   }>
 }
@@ -25,35 +30,51 @@ const Product: FC<ProductProps> = ({ data }) => {
           <div key={node.id}>
             <h2>{intl.formatMessage({ id: node.title })}</h2>
             {node.image && <img src={node.image} alt={node.title} decoding="async" />}
-            <p
-              dangerouslySetInnerHTML={{
-                __html: intl.formatMessage({
-                  id: node.description,
-                }),
-              }}
-            />
-            <h3>
-              {intl.formatMessage({
-                id: 'product_technology_used',
-              })}
-            </h3>
-            <ul>
-              {node.skills.map((skill: string) => {
-                return <li key={skill}>{skill}</li>
-              })}
-            </ul>
+            <p>{intl.formatMessage({ id: node.description })}</p>
+            {node.skills && (
+              <>
+                <h3>{intl.formatMessage({ id: 'product_technology_used' })}</h3>
+                <ul>
+                  {node.skills?.map((s: string) => {
+                    return <li key={s}>{s}</li>
+                  })}
+                </ul>
+              </>
+            )}
             {node.url && (
               <>
-                <h3>
-                  {intl.formatMessage({
-                    id: 'product_url',
-                  })}
-                </h3>
+                <h3>{intl.formatMessage({ id: 'product_url' })}</h3>
                 <p>
                   <a href={node.url} target="_blank" rel="noopener noreferrer">
                     {node.url}
                   </a>
                 </p>
+              </>
+            )}
+            {node.sub && (
+              <>
+                <h3>{intl.formatMessage({ id: 'sub_product' })}</h3>
+                <ul>
+                  {node.sub?.map(
+                    (s: { title: string; skills: string[]; url: string }, index: number) => (
+                      <li key={index}>
+                        <h4>{intl.formatMessage({ id: s.title })}</h4>
+                        <h5>{intl.formatMessage({ id: 'product_technology_used' })}</h5>
+                        <ul>
+                          {s.skills?.map((s: string) => {
+                            return <li key={s}>{s}</li>
+                          })}
+                        </ul>
+                        <h5>{intl.formatMessage({ id: 'product_url' })}</h5>
+                        <p>
+                          <a href={s.url} target="_blank" rel="noopener noreferrer">
+                            {s.url}
+                          </a>
+                        </p>
+                      </li>
+                    ),
+                  )}
+                </ul>
               </>
             )}
           </div>
